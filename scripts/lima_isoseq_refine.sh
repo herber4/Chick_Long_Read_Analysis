e6dc78a0af52a619389d72bcc85cdea1667f2b8d
herber4 <55756028+herber4@users.noreply.github.com>
2026-03-16 12:24:33 -0400

    Add SLURM script for Lima and IsoSeq refinement
    This script is designed to run the Lima and IsoSeq refinement processes on segmented BAM files, utilizing SLURM for job scheduling.

---
+++ scripts/lima_isoseq_refine.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=lima
+#SBATCH -n 32
+#SBATCH --partition=gen-mk-compute-1
+#SBATCH --time=100:00:00
+#SBATCH --mem=64G
+#SBATCH --mail-type=all
+#SBATCH --output=/data2/lackey_lab/DownloadedSequenceData/austin/chick/logs/lima_refine.%j.out
+#SBATCH --error=/data2/lackey_lab/DownloadedSequenceData/austin/chick/logs/lima_refine.%j.err
+#SBATCH --mail-user=herber4@clemson.edu
+# Load change dir
+cd /data2/lackey_lab/DownloadedSequenceData/austin/chick/data/skera_split
+
+source /opt/ohpc/pub/Software/anaconda3/etc/profile.d/conda.sh
+
+conda activate ago
+
+for b in *.segmented.bam ; do
+	N=$(basename $b .segmented.bam) ;
+	lima $b ../../IsoSeq_v2_primers_12.fasta ../lima_out/${N}.lima.bam --isoseq -j 24 --peek-guess ;
+done
+
+cd ../lima_out
+
+for b in *IsoSeqX_3p.bam ; do
+	N=$(basename $b IsoSeqX_3p.bam) ;
+	isoseq refine -j 32 --require-polya $b ../../IsoSeq_v2_primers_12.fasta ../flnc/${N}.flnc.bam ;
+done
