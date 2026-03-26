60ed8407401ab3961376d0564afddec476c46cd2
herber4 <55756028+herber4@users.noreply.github.com>
2026-03-26 11:42:05 -0400

    Update SLURM job parameters in pigeon_classify.sh

--- scripts/pigeon_classify.sh
+++ scripts/pigeon_classify.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=pg_class
 #SBATCH -n 32
-#SBATCH --partition=bigmem
+#SBATCH --partition=gen-mk-compute-1
 #SBATCH --time=100:00:00
-#SBATCH --mem=256G
+#SBATCH --mem=128G
 #SBATCH --mail-type=all
 #SBATCH --output=/data2/lackey_lab/DownloadedSequenceData/austin/chick/logs/pigeon_classify.%j.out
 #SBATCH --error=/data2/lackey_lab/DownloadedSequenceData/austin/chick/logs/pigeon_classify.%j.err
@@ -15,4 +15,4 @@ source /opt/ohpc/pub/Software/anaconda3/etc/profile.d/conda.sh
 
 conda activate isoseq
 
-pigeon classify -j 36 collapsed.sorted.gff galGal6.ncbiRefSeq.sorted.gtf galGal6.fa --fl ../data/mapped/collapsed.flnc_count.txt
+pigeon classify -j 32 collapsed.sorted.gff galGal6.ncbiRefSeq.sorted.gtf galGal6.fa --fl ../data/mapped/collapsed.flnc_count.txt
