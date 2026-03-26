69ae221bb1001d718de0a01d680bbc6b47535e0c
herber4 <55756028+herber4@users.noreply.github.com>
2026-03-26 07:38:34 -0400

    Create isoseq_collapse.sh for SLURM job submission
    Added a SLURM script for collapsing IsoSeq data with specified resources and logging.

---
+++ scripts/isoseq_collapse.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=collapse
+#SBATCH -n 32
+#SBATCH --partition=bigmem
+#SBATCH --time=100:00:00
+#SBATCH --mem=256G
+#SBATCH --mail-type=all
+#SBATCH --output=/data2/lackey_lab/DownloadedSequenceData/austin/chick/logs/isoseq_collapse.%j.out
+#SBATCH --error=/data2/lackey_lab/DownloadedSequenceData/austin/chick/logs/isoseq_collapse.%j.err
+#SBATCH --mail-user=herber4@clemson.edu
+# Load change dir
+cd /data2/lackey_lab/DownloadedSequenceData/austin/chick/data/mapped
+
+source /opt/ohpc/pub/Software/anaconda3/etc/profile.d/conda.sh
+
+conda activate isoseq
+
+isoseq collapse -j 32 --do-not-collapse-extra-5exons mapped_reads_all_samples.bam collapsed.gff
