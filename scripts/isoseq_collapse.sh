22043f0eb1291fd3b1faa442c840fcaede4f858d
herber4 <55756028+herber4@users.noreply.github.com>
2026-03-26 08:21:46 -0400

    Update isoseq collapse command to include flnc.fofn

--- scripts/isoseq_collapse.sh
+++ scripts/isoseq_collapse.sh
@@ -15,4 +15,4 @@ source /opt/ohpc/pub/Software/anaconda3/etc/profile.d/conda.sh
 
 conda activate isoseq
 
-isoseq collapse -j 32 --do-not-collapse-extra-5exons mapped_reads_all_samples.bam collapsed.gff
+isoseq collapse -j 32 --do-not-collapse-extra-5exons mapped_reads_all_samples.bam flnc.fofn collapsed.gff
