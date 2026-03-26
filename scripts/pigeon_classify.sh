d541392ec59519501bb39f979e05fb8186223626
herber4 <55756028+herber4@users.noreply.github.com>
2026-03-26 11:39:03 -0400

    Create pigeon_classify.sh

---
+++ scripts/pigeon_classify.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=pg_class
+#SBATCH -n 32
+#SBATCH --partition=bigmem
+#SBATCH --time=100:00:00
+#SBATCH --mem=256G
+#SBATCH --mail-type=all
+#SBATCH --output=/data2/lackey_lab/DownloadedSequenceData/austin/chick/logs/pigeon_classify.%j.out
+#SBATCH --error=/data2/lackey_lab/DownloadedSequenceData/austin/chick/logs/pigeon_classify.%j.err
+#SBATCH --mail-user=herber4@clemson.edu
+# Load change dir
+cd /data2/lackey_lab/DownloadedSequenceData/austin/chick/pigeon
+
+source /opt/ohpc/pub/Software/anaconda3/etc/profile.d/conda.sh
+
+conda activate isoseq
+
+pigeon classify -j 36 collapsed.sorted.gff galGal6.ncbiRefSeq.sorted.gtf galGal6.fa --fl ../data/mapped/collapsed.flnc_count.txt
